e34ddc28faafcc4273c0f8879eab8d5a8f85d344
asawni20 <abdoaswani1997@gmail.com>
2025-09-27 15:53:35 +0300

    Refactor: Update SignInStates to use a single class
    - Removed sealed class `SignInStates` and its subclasses (`SignInInitState`, `SignInLoadingState`, `SignInSuccessState`, `SignInErrorState`).
    - Consolidated state management into a single `SignInState` class.

--- lib/features/auth/presentation/viewModel/signin/sign_in_states.dart
+++ lib/features/auth/presentation/viewModel/signin/sign_in_states.dart
@@ -1,24 +1,5 @@
 import 'package:flowery_tracking/core/errors/failure.dart';
 import 'package:flowery_tracking/features/auth/domain/entity/signIn/sign_in_response_entity.dart';
-//
-// sealed class SignInStates{}
-//
-// class SignInInitState extends SignInStates{}
-//
-// class SignInLoadingState extends SignInStates{}
-//
-// class SignInSuccessState extends SignInStates{
-//   SignInSuccessState(this.signInResponseEntity);
-//   SignInResponseEntity signInResponseEntity;
-// }
-//
-// class SignInErrorState extends SignInStates{
-//   SignInErrorState(this.message);
-//   final String message;
-// }
-
-
-
 class SignInState {
 
   SignInState({
