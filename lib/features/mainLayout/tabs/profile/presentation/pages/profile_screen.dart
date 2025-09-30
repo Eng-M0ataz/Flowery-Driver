19589c9b6fbb89bf7c0c8fc7051d29a7fd6d0ba9
aswani <abdoaswani1997@gmail.com>
2025-09-30 23:18:52 +0300

    Refactor: Remove unused import from ProfileScreen
    - Removed unused import `app_colors.dart` from `profile_screen.dart`.

--- lib/features/mainLayout/tabs/profile/presentation/pages/profile_screen.dart
+++ lib/features/mainLayout/tabs/profile/presentation/pages/profile_screen.dart
@@ -1,5 +1,4 @@
 import 'package:easy_localization/easy_localization.dart';
-import 'package:flowery_tracking/core/config/theme/app_colors.dart';
 import 'package:flowery_tracking/core/localization/locale_keys.g.dart';
 import 'package:flowery_tracking/core/utils/constants/sizes.dart';
 import 'package:flowery_tracking/features/mainLayout/tabs/profile/presentation/widgets/profile_app_bar.dart';
