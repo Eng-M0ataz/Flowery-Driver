8b2ede5b88695ea8885fac7b8358a24a7962838c
Omar Mohamed <omar.mohamed3029@gmail.com>
2025-10-10 23:02:26 +0300

    Refactor: Rename OrderSuccessScreen to OrderThanksPageScreen
    - Renamed `OrderSuccessScreen` to `OrderThanksPageScreen` for better clarity.
    - Updated the file name from `order_success_page.dart` to `order_thanks_page.dart`.
    - Adjusted the import and class usage in `route_generator.dart` to reflect the name change.

--- lib/features/mainLayout/tabs/success_page/presentation/pages/order_thanks_page.dart
+++ lib/features/mainLayout/tabs/success_page/presentation/pages/order_thanks_page.dart
@@ -6,8 +6,8 @@ import 'package:flowery_tracking/core/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
-class OrderSuccessScreen extends StatelessWidget {
-  const OrderSuccessScreen({super.key});
+class OrderThanksPageScreen extends StatelessWidget {
+  const OrderThanksPageScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
