46b8b96db333b72a8af9a79bd0ad93e6f906a979
aswani <abdelrahmanyoussef97@outlook.com>
2025-10-14 00:24:48 +0300

    feat(orders): Add product card to order details screen
    - Created a new `ProductCard` widget to display product details, including image, title, price, and quantity.
    - Adjusted the layout in `order_details_screen.dart` to incorporate the new product card and improve spacing.

--- lib/features/mainLayout/tabs/orders/presentation/pages/order_details_screen.dart
+++ lib/features/mainLayout/tabs/orders/presentation/pages/order_details_screen.dart
@@ -9,6 +9,7 @@ import 'package:flowery_tracking/features/mainLayout/tabs/orders/presentation/wi
 import 'package:flowery_tracking/features/mainLayout/tabs/orders/presentation/widgets/order_details_text_widget.dart';
 import 'package:flowery_tracking/features/mainLayout/tabs/orders/presentation/widgets/order_state_widget.dart';
 import 'package:flowery_tracking/features/mainLayout/tabs/orders/presentation/widgets/payment_data.dart';
+import 'package:flowery_tracking/features/mainLayout/tabs/orders/presentation/widgets/product_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -35,8 +36,9 @@ class OrderDetailsScreen extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Padding(
-                padding: const EdgeInsets.symmetric(
-                  vertical: AppSizes.spaceBetweenItems_24,
+                padding: const EdgeInsets.only(
+                  top: AppSizes.spaceBetweenItems_24,
+                  bottom: AppSizes.spaceBetweenItems_6
                 ),
                 child: OrderStateWidget(orderNumber: order.order!.orderNumber!, state: order.order!.state!),
               ),
@@ -45,8 +47,10 @@ class OrderDetailsScreen extends StatelessWidget {
               OrderDetailsTextWidget(title: LocaleKeys.userAddress.tr()),
               AddressCard(name: order.order!.user!.firstName!, imagePath: order.order!.user!.photo!, address: order.order!.user!.email!),
               OrderDetailsTextWidget(title: LocaleKeys.orderDetails.tr()),
+              const SizedBox(height: AppSizes.spaceBetweenItems_12,),
               PaymentData(title: LocaleKeys.total.tr(), paymentMethod: '${LocaleKeys.egp.tr()} ${order.order!.totalPrice!}'),
               PaymentData(title: LocaleKeys.paymentMethod.tr(), paymentMethod: order.order!.paymentType!),
+
             ],
           ),
         ),
